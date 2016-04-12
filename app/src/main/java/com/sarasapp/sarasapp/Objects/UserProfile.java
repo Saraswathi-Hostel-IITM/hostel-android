8cd04772513da38675f098e912d158bec3c9e6ad
kevinselvaprasanna <kevinselvaprasanna@gmail.com>
2016-04-13 00:40:16 +0530

    .

--- app/src/main/java/com/sarasapp/sarasapp/Objects/UserProfile.java
+++ app/src/main/java/com/sarasapp/sarasapp/Objects/UserProfile.java
@@ -14,13 +14,30 @@ public class UserProfile {
         editor.putString("name",name);
         editor.commit();
     }
-
-
-
-
+    public static void setEmail(String email,Context context){
+        SharedPreferences preferences = context.getSharedPreferences("sp", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("email",email);
+        editor.commit();
+    }
+    public static void setToken(String token, Context context){
+        SharedPreferences preferences = context.getSharedPreferences("sp", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("token", token);
+        editor.commit();
+    }
     public static String getName(Context context) {
         SharedPreferences pref = context.getSharedPreferences("sp", Context.MODE_PRIVATE);
         return pref.getString("name","");
+    }
+    public static String getEmail(Context context) {
+        SharedPreferences pref = context.getSharedPreferences("sp", Context.MODE_PRIVATE);
+        return pref.getString("email", "");
+    }
+
+    public static String getToken(Context context) {
+        SharedPreferences pref = context.getSharedPreferences("sp", Context.MODE_PRIVATE);
+        return pref.getString("token", "");
     }
 
 
